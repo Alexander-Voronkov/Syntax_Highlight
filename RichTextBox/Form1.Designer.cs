f43c5212a137c440318b612fc5a7d2e00961cc4d
Voro_jn42@student.itstep.org <Voro_jn42@student.itstep.org>
2022-07-04 00:07:13 +0200

    Добавьте файлы проекта.

---
+++ RichTextBox/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace RichTextBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 410);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveAsToolStripMenuItem,
+            this.openToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(38, 22);
+            this.toolStripDropDownButton1.Text = "File";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Text = "Save as";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ RichTextBox/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RichTextBox
+{
+    public partial class Form1 : Form
+    {
+        List<string> KeyWords = new List<string>();
+        string temp = "";
+        public Form1()
+        {
+            InitializeComponent();
+            saveFileDialog1.Title = "Зберегти як";
+            saveFileDialog1.Filter = "rtf | *.rtf";
+            saveFileDialog1.DefaultExt = ".rtf";
+            saveFileDialog1.InitialDirectory = "C:\\";
+            richTextBox1.Font = new Font("Arial",36);
+            LoadKey();
+        }
+        private void LoadKey()
+        {
+            try
+            {
+                using (Stream stream = new FileStream("Key.txt", FileMode.Open))
+                {
+                    using (StreamReader sr=new StreamReader(stream,Encoding.UTF8))
+                    {
+                        KeyWords.AddRange(sr.ReadToEnd().Split(' '));
+                    }
+                }
+            }
+            catch { }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {    
+            if(saveFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                var s = saveFileDialog1.OpenFile();
+                StreamWriter sw = new StreamWriter(s);
+                sw.Write(richTextBox1.Text);
+                sw.Dispose();
+                s.Dispose();
+                
+            }
+            
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var s = openFileDialog1.OpenFile();
+                StreamReader sw = new StreamReader(s);
+                richTextBox1.Text=sw.ReadToEnd();
+                sw.Dispose();
+                s.Dispose();
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (richTextBox1.TextLength > 0)
+            {
+                if(richTextBox1.Text.ToCharArray()[richTextBox1.TextLength - 1]!=' ')
+                temp += richTextBox1.Text[richTextBox1.Text.Length - 1];
+                else if (KeyWords.Contains(temp)&& richTextBox1.Text[richTextBox1.Text.Length - 1] == ' ')
+                {
+                    richTextBox1.Select(richTextBox1.TextLength - temp.Length-1, temp.Length);
+                    richTextBox1.SelectionColor = Color.Blue;
+                    richTextBox1.DeselectAll();
+                    richTextBox1.SelectionColor = Color.Black; 
+                    richTextBox1.Select(richTextBox1.Text.Length, 0);
+                    temp = "";
+                }
+                else if(!KeyWords.Contains(temp) && richTextBox1.Text[richTextBox1.Text.Length - 1] == ' ')
+                {
+                    temp = "";
+                }
+            }
+
+        }
+    }
+}
